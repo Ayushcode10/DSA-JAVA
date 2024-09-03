0332c264a93c4b0dd265b438da1d36009574f470
Ayushcode10 <ayushh.code@gmail.com>
2024-09-04 00:13:54 +0530

    new commit

---
+++ src/com/OOPS/Singleton/SubClass.java
@@ -0,0 +1,27 @@
+//package com.OOPS.Singleton;
+//
+//import com.OOPS.AccessControls.A;
+////for A class in AccessContols package for protected example
+//public class SubClass extends A {
+//    public SubClass(int num ,String s){
+//        super(num,s);
+//
+//    }
+//    public static void main(String[] args) {
+//        SubClass obj = new SubClass(1,"ayush");
+//        int n = obj.num;
+//        System.out.println(n);
+//    }
+//}
+////for A class in AccessContols package for protected example
+//public class SubClass2 extends A {
+//    public SubClass2(int num ,String s){
+//        super(num,s);
+//
+//    }
+//    public static void main(String[] args) {
+//        SubClass2 obj = new SubClass2(1,"ayush");
+//        int n = obj.num;
+//        System.out.println(n);
+//    }
+//}
