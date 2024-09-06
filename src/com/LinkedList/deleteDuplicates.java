0201d355ffbd2dfc7f89b170fdb608f7dae5d462
Ayushcode10 <ayushh.code@gmail.com>
2024-09-06 17:06:05 +0530

    new commit

--- src/com/LinkedList/deleteDuplicates.java
+++ src/com/LinkedList/deleteDuplicates.java
@@ -16,6 +16,32 @@ public class deleteDuplicates {
         size++;
     }
 
+    //method to delete duplicate elements
+    //O(N) complexity, because we are traversing the list
+    public void DeleteDuplicates(){
+        //create a temp iterator to traverse the list
+        Node temp = head;
+        //loop runs till the next link of the last element points to null
+        while(temp.next != null){
+            //check if the value of two consecutive elements are equal
+            if(temp.data == temp.next.data){
+            //if equal, break the link and make the next link of the node to point to the
+            //node after the equal node
+                temp.next = temp.next.next;
+                //decrease the size if breaking the link
+                size--;
+            }
+            else {
+                //if not equal, simply update the iterator
+                temp = temp.next;
+            }
+        }
+        //after temp reaches last element, make the tail point to it
+        tail = temp;
+        //the next link of the tail node will always point to null
+        tail.next = null;
+    }
+
     public void print(){
         Node temp = head;
         while(temp != null){
@@ -27,11 +53,14 @@ public class deleteDuplicates {
 
     public static void main(String[] args) {
         deleteDuplicates list = new deleteDuplicates();
-        list.add(5);
-        list.add(4);
         list.add(3);
         list.add(2);
+        list.add(2);
         list.add(1);
+        list.add(1);
+        list.print();
+        System.out.println("\nAfter Removing duplicate values-->");
+        list.DeleteDuplicates();
         list.print();
     }
     private class Node{
