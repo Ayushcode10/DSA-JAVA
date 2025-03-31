b479c8d77fe748e0d768335e7a664c351092813d
Ayushcode10 <ayushh.code@gmail.com>
2025-03-31 14:34:51 +0530

    new commit

--- src/com/Recursion/Strings.java
+++ src/com/Recursion/Strings.java
@@ -2,13 +2,12 @@ package com.Recursion;
 
 public class Strings {
     public static void main(String[] args) {
-//        String s = "baccad";
-//        String a = skipChar(s,"");
-//        System.out.println(a);
-//    }
+        String s = "bsappleabs";
+        System.out.println(skipString(s));
+
     }
 
-    public static void skipChar(String s,String ans){
+    public static void skipChar(String p,String up){
 //        StringBuilder sb = new StringBuilder();
 //        for (int i = 0; i < s.length(); i++) {
 //            if(s.charAt(i) != skip){
@@ -16,13 +15,29 @@ public class Strings {
 //            }
 //        }
 //        return sb.toString();
-//        if(ans.isEmpty()){
-//            System.out.println(s);
-//            return;
-//        }
-//        String s =
-//        return skipChar(s,ans);
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+        char ch = up.charAt(0);
+        if(ch == 'a'){
+            skipChar(p,up.substring(1));
+        }
+        else{
+            skipChar(p+ch,up.substring((1)));
+        }
+    }
 
+    public static String skipString(String s){
+        if(s.isEmpty()){
+            return "";
+        }
+        if(s.startsWith("apple")){
+            return skipString(s.substring(5));
+        }
+        else {
+            return s.charAt(0) + skipString(s.substring(1));
+        }
     }
 
 }
